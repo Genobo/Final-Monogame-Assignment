b1c69a5b7fac9c6e11c9b86012f91cf4403be98a
Gabriel Lindsay <339516205@GoTVDSB.ca>
2024-01-15 11:01:09 -0500

    added scope texture

--- Game1.cs
+++ Game1.cs
@@ -11,6 +11,18 @@ namespace Final_Monogame_Assignment
         Texture2D duckTexture;
         Rectangle duckRect;
         Vector2 duckSpeed;
+        MouseState mouseState;
+        enum Screen
+        {
+            Intro,
+            RubberDuck
+        }
+        Screen screen;
+        Texture2D duckIntroTexture;
+        Texture2D scopeTexture;
+        Rectangle scopeLocation;
+        Texture2D fieldTexture;
+        KeyboardState keyboardState;
 
         public Game1()
         {
@@ -25,13 +37,18 @@ namespace Final_Monogame_Assignment
 
             base.Initialize();
             duckRect = new Rectangle(300, 10, 100, 100);
-            duckSpeed = new Vector2(2, 2);
+            duckSpeed = new Vector2(4, 5);
+            scopeLocation = new Rectangle(10, 10, 75, 75);
+            screen = Screen.Intro;
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            duckTexture = Content.Load<Texture2D>("rubberDuck");
+            duckTexture = Content.Load<Texture2D>("rubber-duck2");
+            duckIntroTexture = Content.Load<Texture2D>("Duck_Hunt");
+            scopeTexture = Content.Load<Texture2D>("reticle2");
+            fieldTexture = Content.Load<Texture2D>("duck-hunt-11");
 
             // TODO: use this.Content to load your game content here
         }
@@ -44,6 +61,25 @@ namespace Final_Monogame_Assignment
             // TODO: Add your update logic here
 
             base.Update(gameTime);
+            keyboardState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+            if (screen == Screen.Intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                {
+                    screen = Screen.RubberDuck;
+                }
+            }
+            duckRect.X += (int)duckSpeed.X;
+            duckRect.Y += (int)duckSpeed.Y;
+            if (duckRect.Right > _graphics.PreferredBackBufferWidth || duckRect.Left < 0)
+            {
+                duckSpeed.X *= -1;
+            }
+            if (duckRect.Bottom > _graphics.PreferredBackBufferHeight || duckRect.Top < 0)
+            {
+                duckSpeed.Y *= -1;
+            }
         }
 
         protected override void Draw(GameTime gameTime)
@@ -53,6 +89,18 @@ namespace Final_Monogame_Assignment
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+            _spriteBatch.Begin();
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(duckIntroTexture, new Rectangle(0, 0, 800, 500), Color.White);
+            }
+            else if (screen == Screen.RubberDuck)
+            {
+                _spriteBatch.Draw(fieldTexture, new Rectangle(0, 0, 800, 500), Color.White);
+                _spriteBatch.Draw(duckTexture, duckRect, Color.White);
+                _spriteBatch.Draw(scopeTexture, new Rectangle(300, 10, 100, 100), Color.White);
+            }
+            _spriteBatch.End();
         }
     }
 }
---
+++ Player.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Monogame_Assignment
+{
+    public class Player
+    {
+        private Texture2D _texture;
+        private Rectangle _location;
+        private Vector2 _speed;
+    }
+}
